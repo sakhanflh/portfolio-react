524c0d717ea67120006237e0c5ffa2cf460ecf0a
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-03-13 21:08:25 +0700

    try to fixed bug

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -7,10 +7,10 @@ export default function HomePage(){
 
     return(
         <>
-        <div>
+        <div className="bg-black w-full h-dvh">
             <Header/>
 
-            <div className="bg-black w-full h-dvh text-white pt-32 px-5 flex flex-col items-center gap-5">
+            <div className="w-full text-white pt-32 px-5 flex flex-col items-center gap-5">
                 <div className="w-60 h-60 rounded-full border-2 border-textcolor overflow-hidden">
                     <img src="/img/Sakha2.jpg" alt="" className="object-cover w-full h-full"/>
                 </div>
